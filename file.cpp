598c25f8a5db036e434ed48d75de6859f7332dfb
hoorain-sundus <hoorain.sunduss@gmail.com>
2025-02-14 15:01:20 +0500

    Sorting

--- file.cpp
+++ file.cpp
@@ -961,6 +961,8 @@
 //   }
 
 
+//---------------------------- Bubble sorting ------------------------------------------
+
 
 void Sorting(int arr[], int size)
 {
@@ -995,4 +997,49 @@ int main()
     return 0;
 }
 
+
+
+//----------------------Bubble Sorting with Dynamic Input -------------------------------
+//-----------------------------(In Ascending Order)------------------------------------------
+
+// void Bubblesort(int arr[], int n){
+
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         for (int j = 0; j < n - i - 1; j++)
+//         {
+//             if (arr[j] > arr[j + 1])
+//             {
+//                arr[j] = arr[j] + arr[j + 1];
+//                 arr[j + 1] = arr[j] - arr[j + 1];
+//                 arr[j] = arr[j] - arr[j + 1];
+//             }
+            
+//         }
+        
+//     }
+
+// }
+
+
+// int main (){
+
+//     int n ;
+//     cout << "enter size of array: ";
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+    
+//       Bubblesort(arr,n);
+//       for (int i = 0; i < n; i++)
+//       {
+//         cout << arr[i] << " ";
+//       }
+//       cout << "\n";
+//       return 0;
+// }
 
