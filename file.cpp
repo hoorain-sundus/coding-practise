0947cd3d971637569057aa0898825efdc71a25e1
hoorain-sundus <hoorain.sunduss@gmail.com>
2025-02-15 15:35:01 +0500

    Sorting

--- file.cpp
+++ file.cpp
@@ -1043,3 +1043,50 @@ int main()
 //       return 0;
 // }
 
+
+//------------------------ Bubble Sorting in Descending Order -----------------------------------------------
+
+
+
+
+// void Bubblesort(int arr[], int n){
+
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         for (int j = 0; j < n - i - 1 ; j++)
+//         {
+//             if (arr[j] < arr[j + 1])
+//             {
+//                 arr[j + 1] = arr[j + 1] + arr[j];
+//                 arr[j] = arr[j + 1] - arr[j];
+//                 arr[j + 1] = arr[j + 1] - arr[j];
+//             }
+            
+//         }
+        
+//     }
+
+// }
+
+
+// int main (){
+
+//     int n ;
+//     cout << "enter size of array: ";
+//     cin >> n;
+
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+    
+//       Bubblesort(arr,n);
+//       for (int i = 0; i < n; i++)
+//       {
+//         cout << arr[i] << " ";
+//       }
+//       cout << "\n";
+//       return 0;
+// }
+
