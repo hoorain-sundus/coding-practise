4243180d342b9f510926ee4f06c0ea419f749181
hoorain-sundus <hoorain.sunduss@gmail.com>
2025-02-16 16:05:00 +0500

    Sorting

--- file.cpp
+++ file.cpp
@@ -1090,3 +1090,39 @@ int main()
 //       return 0;
 // }
 
+
+// ---------------------- Bubble Sorting With Temporary variable -------------------------------
+
+
+// void Sorting(int arr[], int size)
+// {
+//     for (int i = 0; i < size - 1; i++)
+//     {
+//         for (int j = 0; j < size - i - 1; j++)
+//         {
+//             if (arr[j] > arr[j + 1])
+//             {
+//                 int temp = arr[j + 1];
+//                 arr[j + 1] = arr[j];
+//                 arr[j] = temp; 
+//             }
+//         }
+//     }
+// }
+
+// int main()
+// {
+
+//     int arr[] = {3, 6, 90, 2, 0, 74};
+//     int size = sizeof(arr) / sizeof(arr[0]);
+
+//     Sorting(arr, size);
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         cout << arr[i] << " ";
+//     }
+//     cout << "\n";
+
+//     return 0;
+// }
