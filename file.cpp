b47bfe4f74e1ab5bee6f2306692051c65cb4cb5f
hoorain-sundus <hoorain.sunduss@gmail.com>
2025-03-19 20:07:56 +0500

    Activity

--- file.cpp
+++ file.cpp
@@ -1355,3 +1355,37 @@
     
 //     return 0;
 // }
+
+
+// ------------------------- ACTIVITY NO : 11 --------------------------------------
+// ------------------- Program to read 10 numbers and sort them into descending order ------------------------------
+
+// int main()
+// {
+
+//     int arr[6] = { 2, 3, 1, 4, 6, 5 };
+
+//     for (int i = 0; i < 6 -1; i++)
+//     {
+
+//         for (int j = 0; j < 6 -i -1; j++) 
+//         {
+//             if (arr[j] < arr[j + 1])
+//             {
+//                 int temp = arr[j + 1];
+//                 arr[j + 1] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+
+//     }
+
+   
+//     for (int i = 0; i < 6; i++)
+//     {
+
+//         cout << arr[i];
+//     }
+
+//     return 0;
+// }
